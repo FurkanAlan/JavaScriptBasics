d9254bfdebb5371837c9e62317054b4160e00023
FurkanAlan <ulkirr@gmail.com>
2020-07-07 15:58:28 -0500

    Functions module added

--- 5.class/functionExportImports/functionImporter.js
+++ 5.class/functionExportImports/functionImporter.js
@@ -1,6 +1,6 @@
 // import * as basicCalculation from "./nameExporter"
 
-
+console.log("================================= 1. method ===================================")
 const modName = require("./functionExporter")
 // const {subTwo} = require("./nameExporter");
 
@@ -8,6 +8,24 @@ console.log(modName.subTwo(4,1))
 console.log(modName.mulTwo(4,3))
 console.log(modName.divTwo(4,2))
 console.log(modName.PI)
+
+console.log("================================= 2. method ===================================")
+
+const {subTwo,mulTwo} = require("./functionExporter")  // bu skilde sadece istedigimiz ifadeleri kullanmis oluruz
+console.log(subTwo(2,3))
+
+
+console.log("================================= to get from different package ===================================")
+
+//importing from other packages
+// const modFarImporter = require("./moduleExample/indexMain")
+const modFarImporter = require("./moduleExample") //in here I am pointing a folder and than node will look inside of that folder and look for indMain.js file, initially will check the package.json what is the entry of main
+//core node module de boyle birsey yapmaya gerek kalmiyoe mesela fileserve almak icin const fs = require("fs") yereli olur
+
+
+modFarImporter()
+
+
 
 //
 //
---
+++ 5.class/functionExportImports/moduleExample/indexMain.js
@@ -0,0 +1,6 @@
+const y = function (){
+    console.log("this is a function y")
+}
+
+const YPI = 3.14
+module.exports = y
