b265548572763462399ba6e5e04f55d5ba395395
FurkanAlan <ulkirr@gmail.com>
2020-07-07 20:48:25 -0500

    Functions module added

--- 5.class/ExperimentalFeatures/expFeatureExporter.js
+++ 5.class/ExperimentalFeatures/expFeatureExporter.js
@@ -1,8 +1,79 @@
+console.log("================================== subtraction of two numbers =========================================")
+
 export function subTwo(number1, number2) {
     return number1 - number2;
 }
 
+console.log("================================== addition of two numbers =========================================")
 
 export function addTwo(number1, number2) {
     return number1 + number2;
 }
+
+console.log("=================================== ordered numbers ========================================")
+
+
+const sort = (...args) => args.sort((a, b) => a - b)
+
+console.log("================================== reverse ordered number =========================================")
+
+
+function reverseSort(...args) {
+    return args.sort(((a, b) => b - a))
+}
+
+console.log("==================================== PI example =======================================")
+
+
+const PI = 3.1
+console.log("=================================== clone example ========================================")
+
+
+const clone = (obj) => ({...obj})
+
+console.log("================================ export class example ===========================================")
+
+export class Vehicle {
+    constructor(tires) {
+        this.tires = tires
+    }
+
+    get getTires() {
+        return this.tires
+    }
+}
+
+console.log("===========================================================================")
+// export default const person = {name: "mehmet"}//bu ifade hataya swbwp oluyor
+//bunu yapabilmek icin
+const person = {name: "mehmet"}
+// export default person  //bu seferde bize bir yerde iki tane export default olmaz diyecek
+
+// export default () => ({name : "Kartal"}) // bunun adi anonymous function
+
+
+//Sadece bir tane export default a sistem izin veriyor mesela tumunu export default olarak toplayabiliriz
+
+export {reverseSort, sort}
+export {PI}
+export {clone as cloneAnyObjectShallow}  //bunu kullancagimiz zaman cloneAnyObjectShallow bu isimle kullanmaliyiz
+
+export default Vehicle
+
+
+
+console.log("================================== export deault all, sadece 1 tane deault kulllanilabili r=========================================")
+console.log("export default un faydasi { } parantezi import Truck from ./expFeatureExporter.js import dan kaldiiriyor")
+
+// export default {
+//     PI, reverseSort
+// }
+
+console.log("================================== 2. yontem exportlar bir yerde =========================================== ")
+
+const allUtils = {
+    PI, reverseSort, sort, addTwo, subTwo
+}
+// export {allUtils as default, clone } //bu dogru olmayabilir
+
+// export * from "./expFeatureImporter"
--- 5.class/ExperimentalFeatures/expFeatureImporter.js
+++ 5.class/ExperimentalFeatures/expFeatureImporter.js
@@ -1,12 +1,64 @@
+import * as utils from "./expFeatureExporter.js";
 import * as twoNumberCalc from "./expFeatureExporter.js"
+import {subTwo, addTwo} from "./expFeatureExporter.js";  //second way
+import {reverseSort, sort, PI as numberPI} from "./expFeatureExporter.js";
+import {cloneAnyObjectShallow} from "./expFeatureExporter.js";
 
-import {subTwo,addTwo} from "./expFeatureExporter.js";  //second way
+import Truck from "./expFeatureExporter.js";
+// import allItems from "./expFeatureExporter.js";
+// import getName from "expFeatureExporter.js"
 
-console.log(twoNumberCalc.subTwo(12,4))
+// import "./expFeatureExporter.js" // bu metod cok guvenli olmayabilir
 
-console.log(subTwo(5,20))
+// import * as xUtil from "./expFeatureExporter"
 
-console.log(addTwo(5,20))
+console.log("================================== subtraction of two numbers =========================================")
+
+console.log(twoNumberCalc.subTwo(12, 4))
+
+console.log("================================== subtraction of two numbers  2. way=========================================")
+
+
+console.log(subTwo(5, 20))
+
+console.log("================================== addition of two numbers =========================================")
+
+
+console.log(addTwo(5, 20))
+
+console.log("================================== PI =========================================")
+
+
+console.log(numberPI)
+
+console.log("================================== normal ordered number =========================================")
 
 
 
+console.log(sort(12, 4, 5, 6, 7, 89, 9, 99, 0, 1))
+
+console.log("================================== reverser ordered =========================================")
+
+
+console.log(reverseSort(-12, 4, 45, numberPI * 12, 3, 0))
+
+console.log("================================== clone ornegi =========================================")
+
+console.log(cloneAnyObjectShallow({name: "Ali", street: "Istanbul mahallesi"}))
+
+
+console.log("================================== reverser ordered ikinci import metoduyla=========================================")
+
+
+console.log(utils.reverseSort(-12, 4, 5, 6, 8, 9, 0, -1, -12))
+
+
+console.log("================================== class export ornegi =========================================")
+
+console.log(new Truck(18))
+
+
+// console.log(getPersonName())
+
+// console.log(allItems.reverseSort(12,3,4,7,8,9,900))
+// console.log(allItems.PI)
